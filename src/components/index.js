2eed7c4ff1a20a4ec1adfc666130972359e966b9
gaurav3278 <gaurav.rai2020@vitbhopal.ac.in>
2022-05-29 10:55:24 +0530

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import Hero from "./Hero/Hero";
 import Body from "./Body/Body";
 import Header from "./Header/Header";
-import Review from './Review/Review'
 
 export{
     Hero,
     Body,
     Header,
-    Review
 };
